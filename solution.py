16158ac516194949ac862cae759a1305900b0838
kotarena <129864371+kotarena@users.noreply.github.com>
2023-04-05 22:26:02 +0500

    1

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5334335970 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return math.sqrt(2 / ((x**2).mean() - (x.mean())**2)) # Ваш ответ
